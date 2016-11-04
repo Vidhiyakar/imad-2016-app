84989fcb85064cafa8e57d1983aca8c91b67d05c
Senthil Vidhiyakar <svidhiyakar@gmail.com>
2016-11-04 23:33:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,7 +13,7 @@ request.onreadystatechange=function()
 };
 request.open('GET','http://vidhiyakar.imad.hasura-app.io/pagevisited',true);
 request.send(null);
-}
+
 var login = document.getElementById('login');
 login.onclick= function(){
     var request=new XMLHttpRequest();
@@ -28,13 +28,12 @@ login.onclick= function(){
                 console.log('invalid');
                 alert('invalid');
             }
-        }};
+        }}
     var username= document.getElementById('username').value;
     var password= document.getElementById('password').value;
     request.open('GET','http://vidhiyakar.imad.hasura-app.io/logincheck/'+(username+'$'+password),true);
     request.send(null);
 }
-`
 
 window.onclick = function(event) {
   if (!event.target.matches('.dropbtn')) {
