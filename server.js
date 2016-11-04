9280f5fdb7ed37d900357fc0aba8d0110d7efabb
Senthil Vidhiyakar <svidhiyakar@gmail.com>
2016-11-04 16:49:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -32,9 +32,7 @@ app.get('/pagevisited', function (req, res) {
     }
     else
     {
-        var jsonstring = JSON.stringify(result.rows);
-        var array=JSON.parse(jsonstring);
-        res.send(arr[0]+" is "+isNan(arr[0]));
+        res.send(JSON.stringify(result.rows));
     }
     });
 });
