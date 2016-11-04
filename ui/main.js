53dda36cc6bfdcd912b104ef2e725cbabecf7cfe
Senthil Vidhiyakar <svidhiyakar@gmail.com>
2016-11-04 17:23:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,20 @@
 function setPageVisits(){
-    
+    var counterElement = document.getElementById('counter');
+    var request=new XMLHttpRequest();
+    request.onreadystatechange=function()
+    {
+        if(request.readyState===XMLHttpRequest.DONE)
+        {
+            if(request.status===200)
+            {
+                var counter =request.responseText;
+                var span=document.getElementById('count');
+                counterElement.innerHTML=counter.toString();
+            }
+        }
+    };
+    request.open('GET','http://vidhiyakar.imad.hasura-app.io/pagevisited',true);
+    request.send(null);
+
 }
 setPageVisites();
