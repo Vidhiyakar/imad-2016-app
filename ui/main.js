bc15b77072ae5ae8ecdbf66fd6fc377d3ad8854f
Senthil Vidhiyakar <svidhiyakar@gmail.com>
2016-11-04 22:02:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -35,5 +35,5 @@ login.onclick= function(){
     console.log(password);
     request.open('POST','http://vidhiyakar.imad.hasura-app.io/login',true);
     request.setRequestHeader('Content-Type','application/json');
-    request.send(JSON.stringify({username:username,password:password}));
+    request.send(JSON.stringify({"username":username,"password":password}));
 }
