9e6ed675ecefa0fdc782737378a6c0befd0a1ce4
Senthil Vidhiyakar <svidhiyakar@gmail.com>
2016-11-04 15:47:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool=require('pg').Pool;
-
+var pool=new Pool(db_config);
 var db_config = {
   host: 'db.imad.hasura-app.io',
   user: 'vidhiyakar',
