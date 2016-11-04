02b6210744763e6c3670c8e4bfb00ffbe3028774
Senthil Vidhiyakar <svidhiyakar@gmail.com>
2016-11-04 17:26:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,19 +1,15 @@
-function setPageVisits(){
-    var counterElement = document.getElementById('counter');
-    var request=new XMLHttpRequest();
-    request.onreadystatechange=function()
+var counterElement = document.getElementById('counter');
+var request=new XMLHttpRequest();
+request.onreadystatechange=function()
+{
+    if(request.readyState===XMLHttpRequest.DONE)
     {
-        if(request.readyState===XMLHttpRequest.DONE)
+        if(request.status===200)
         {
-            if(request.status===200)
-            {
-                var counter=request.responseText;
-                counterElement.innerHTML=counter.toString();
-            }
+            var counter=request.responseText;
+            counterElement.innerHTML=counter.toString();
         }
-    };
-    request.open('GET','http://vidhiyakar.imad.hasura-app.io/pagevisited',true);
-    request.send(null);
-
-}
-setPageVisites();
+    }
+};
+request.open('GET','http://vidhiyakar.imad.hasura-app.io/pagevisited',true);
+request.send(null);
