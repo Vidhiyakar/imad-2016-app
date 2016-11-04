5fabe4338fe7de8abc11a219ac4d72525e28de45
Senthil Vidhiyakar <svidhiyakar@gmail.com>
2016-11-04 16:19:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,15 +12,6 @@ var db_config = {
 var pool = new Pool(db_config);
 var app = express();
 app.use(morgan('combined'));
-function updatePageVisit(count){
-    pool.query("update info set value='"+count+"' where field='visitcount'",function(err,result)
-    {
-    if(err)
-    {
-        res.status(500).send(err.toString());
-    }
-    });
-}
 app.get('/pagevisited', function (req, res) {
     pool.query("SELECT value FROM info where field='visitcount'",function(err,result)
     {
@@ -32,7 +23,6 @@ app.get('/pagevisited', function (req, res) {
     {
         var visitcount=Integer.parseInt(result.rows[0].value);
         visitcount++;
-        updatePageVisit(visitcount);
         res.send(visitcount);
     }
     });
