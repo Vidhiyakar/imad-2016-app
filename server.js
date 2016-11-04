a605aeb653e38c421fbfd3e4b98a3a5b87c2c8cd
Senthil Vidhiyakar <svidhiyakar@gmail.com>
2016-11-04 19:56:49 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,68 @@
+var express = require('express');
+var morgan = require('morgan');
+var path = require('path');
+var Pool=require('pg').Pool;
+
+var db_config = {
+  host: 'db.imad.hasura-app.io',
+  user: 'vidhiyakar',
+  password: process.env.DB_PASSWORD,
+  database: 'vidhiyakar',
+  port: '5432'
+};
+var pool = new Pool(db_config);
+var app = express();
+
+app.use(morgan('combined'));
+
+var updatePageVisit=function(count){
+    count++;
+    pool.query("update info set value='"+count.toString()+"' where field='visitcount'",function(err,result)
+    {
+       if(err){
+           res.send("error");
+       } 
+    });
+}
+app.get('/pagevisited', function (req, res) {
+    pool.query("SELECT value FROM info where field='visitcount'",function(err,result)
+    {
+    if(err)
+    {
+        res.status(500).send(err.toString());
+    }
+    else
+    {
+        var visitcount=result.rows[0].value;
+        updatePageVisit(visitcount);
+        res.send(visitcount);
+    }
+    });
+});
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+app.get('/bgp.jpg',function(req, res){
+   res.sendFile(path.join(__dirname, 'ui', 'bgp.jpg')); 
+});
+
+app.get('/dp.png',function(req, res){
+   res.sendFile(path.join(__dirname, 'ui', 'dp.png'));
+});
+
+app.get('/logo.png',function(req, res){
+   res.sendFile(path.join(__dirname, 'ui', 'logo.png'));
+});
+
+app.get('/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+app.get('/main.js', function(req,res){
+   res.sendFile(path.join(__dirname,'ui','main.js'));
+});
+var port = 8080; // Use 8080 for local development because you might already have apache running on 80
+app.listen(8080, function () {
+  console.log(`IMAD course app listening on port ${port}!`);
+});
