71615e37d5032b4f3ddbd60e89dca0a1dfb8ae4c
Senthil Vidhiyakar <svidhiyakar@gmail.com>
2016-11-04 21:41:07 +0530

    Update main.js

--- ui/main.js
+++ ui/main.js
@@ -15,3 +15,17 @@ request.open('GET','http://vidhiyakar.imad.hasura-app.io/pagevisited',true);
 request.send(null);
 
 var login = document.getElementById('login');
+login.onclick= function(){
+    var request=new XMLHttpRequest();
+    request.onreadystatechange=function(){
+        if(request.readyState===XMLHttpRequest.DONE)
+        {        
+            if(request.status===200)
+            {            
+                var counter=request.responseText;
+                counterElement.innerHTML=counter.toString();
+            }    
+        }};
+    request.open('GET','http://vidhiyakar.imad.hasura-app.io/pagevisited',true);
+    request.send(null);
+}
