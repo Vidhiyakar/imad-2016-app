3e4c48aeca811a3074bd5d9f35f83c17e1eafb78
Senthil Vidhiyakar <svidhiyakar@gmail.com>
2016-11-04 13:10:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.get('/', function (req, res) {
 app.get('/bgp.jpg',function(req, res){
    res.sendFile(path.join(__dirname, 'ui', 'bgp.jpg')); 
 });
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
