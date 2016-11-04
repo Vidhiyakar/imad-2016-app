74a6efad16820818b019f46ab7f999b581589d27
Senthil Vidhiyakar <svidhiyakar@gmail.com>
2016-11-04 23:26:34 +0530

    Add files via upload

--- ui/main.js
+++ ui/main.js
@@ -34,3 +34,18 @@ login.onclick= function(){
     request.open('GET','http://vidhiyakar.imad.hasura-app.io/logincheck/'+(username+'$'+password),true);
     request.send(null);
 }
+`
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
