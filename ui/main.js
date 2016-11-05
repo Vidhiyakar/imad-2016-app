50d0a6309b6a7e555002b7cdf8a9e0720c2c0d88
Senthil Vidhiyakar <svidhiyakar@gmail.com>
2016-11-05 07:56:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,7 +13,7 @@ request1.onreadystatechange=function()
         if(request1.status===403)
         {
             var responseString = request1.responseText;
-            loginform.innertHTML = 'senthil';
+            loginform.innertHTML = responseString.toString();
         }
     }
 };
