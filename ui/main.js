08d61a9eb3e21e64a62d4df7be261fe244ff67c5
Senthil Vidhiyakar <svidhiyakar@gmail.com>
2016-11-05 07:52:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,7 +12,7 @@ request1.onreadystatechange=function()
         if(request1.status===403)
         {
             var responseString = request.responseText;
-            loginform.innertHtml = responseString.toString();
+            loginform.innertHTML = responseString.toString();
         }
     }
 };
