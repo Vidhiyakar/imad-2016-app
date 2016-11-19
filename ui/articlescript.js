04724f4336741d29544eb3c205a568156e7d6d13
Senthil Vidhiyakar <svidhiyakar@gmail.com>
2016-11-19 11:30:55 +0530

    [imad-console] Updates ui/articlescript.js

--- ui/articlescript.js
+++ ui/articlescript.js
@@ -1,4 +1,7 @@
 var addarticles=function(){
-  var title = document.getElementById('title').value;
-  var content = document.getElementById('content').value;
+  var title = document.getElementById('title').value.toString();
+  var content = document.getElementById('content').value.toString();
+  var date = new Date();
+  var datestring=date.getFullYear()+"-"+date.getMonth()+"-"+date.getDate(); 
+  
 };
