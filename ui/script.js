c8ffa21d030b6d1231fb1ac9674029ae5b3bfbd3
Senthil Vidhiyakar <svidhiyakar@gmail.com>
2016-11-23 22:19:28 +0530

    [imad-console] Updates ui/script.js

--- ui/script.js
+++ ui/script.js
@@ -61,25 +61,29 @@ function addComment(){
 }
 
 function addArticles(){
-  var title = document.getElementById('title').value.toString();
-  var content = document.getElementById('content').value.toString();
-  var date = new Date();
-  var datestring=date.getFullYear()+"-"+(date.getMonth()+1)+"-"+date.getDate(); 
-  var request=new XMLHttpRequest();
-  request.onreadystatechange=function()
-  {
-    if(request.readyState===XMLHttpRequest.DONE)
-    {
-        if(request.status===200)
-        { 
-            document.getElementById('title').value="";
-            document.getElementById('content').value="";
-            window.alert('Post successfull');
-        }else{
-            window.alert(request.responseText);
+  if(loggedinFlag === true){
+      var title = document.getElementById('title').value.toString();
+      var content = document.getElementById('content').value.toString();
+      var date = new Date();
+      var datestring=date.getFullYear()+"-"+(date.getMonth()+1)+"-"+date.getDate(); 
+      var request=new XMLHttpRequest();
+      request.onreadystatechange=function()
+      {
+        if(request.readyState===XMLHttpRequest.DONE)
+        {
+            if(request.status===200)
+            { 
+                document.getElementById('title').value="";
+                document.getElementById('content').value="";
+                window.alert('Post successfull');
+            }else{
+                window.alert(request.responseText);
+            }
         }
-    }
-  };
-  request.open('GET','http://vidhiyakar.imad.hasura-app.io/blogit/'+title+"/"+content+"/"+datestring,true);
-  request.send(null);
+      };
+      request.open('GET','http://vidhiyakar.imad.hasura-app.io/blogit/'+title+"/"+content+"/"+datestring,true);
+      request.send(null);
+  }else{
+      window.alert('Please login to Post on my Timeline');
+  }
 }
