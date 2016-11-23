c2d2804cad04fdd3fc22b1e297dd4a9795a9fd60
Senthil Vidhiyakar <svidhiyakar@gmail.com>
2016-11-23 06:24:48 -0800

    [imad-console] Updates ui/script.js

--- ui/script.js
+++ ui/script.js
@@ -1,1 +1,23 @@
-
+function addArticles(){
+  var title = document.getElementById('title').value.toString();
+  var content = document.getElementById('content').value.toString();
+  var date = new Date();
+  var datestring=date.getFullYear()+"-"+date.getMonth()+"-"+date.getDate(); 
+  var request=new XMLHttpRequest();
+  request.onreadystatechange=function()
+  {
+    if(request.readyState===XMLHttpRequest.DONE)
+    {
+        if(request.status===200)
+        { 
+            document.getElementById('title').value="";
+            document.getElementById('content').value="";
+            window.alert('Post successfull');
+        }else{
+            window.alert(request.responseText);
+        }
+    }
+  };
+  request.open('GET','http://vidhiyakar.imad.hasura-app.io/blogit/'+title+"/"+content+"/"+datestring,true);
+  request.send(null);
+}
