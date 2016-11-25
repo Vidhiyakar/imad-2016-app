80953f1882999822c9e2b33f609cd11ba4fcc1fa
Senthil Vidhiyakar <svidhiyakar@gmail.com>
2016-11-25 07:30:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ app.get('/articles',function(req,res){
            var count=result.rows.length;
            var timelineString="";
            for(var x=0;x<count;x++){
-               var dateString=result.rows[x].date.substring(4,15);
+               var dateString=result.rows[x].date.toString().substring(4,15);
                timelineString+=""+result.rows[x].article_id+","+result.rows[x].author_id+","+result.rows[x].username+","+result.rows[x].title+","+result.rows[x].content+","+dateString;
            }
            res.status(200).send(""+timelineString);
