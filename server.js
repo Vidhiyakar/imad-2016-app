a1fc87d1b5b2e69d2a0b56d1bdfbaaeecfdab3e6
Senthil Vidhiyakar <svidhiyakar@gmail.com>
2016-11-25 07:08:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ app.get('/getcomments',function(req,res){
 });
 
 app.get('/getarticles',function(req,res){
-    pool.query("select article_id,author_id,username,title,content,date from articles a, users u where a.author_id=u.id",function(err,result){
+    pool.query("select a.article_id,a.author_id,u.username,a.title,a.content,a.date from articles a, users u where a.author_id=u.id",function(err,result){
        if(err){
            res.send(""+err.toString());
        }else{
