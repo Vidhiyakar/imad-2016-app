efe000b49573a9eb948aa5cc207c837e19bb4712
Senthil Vidhiyakar <svidhiyakar@gmail.com>
2016-11-26 09:03:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -95,7 +95,7 @@ app.get('/articlecomment/:artcile_id',function(req,res){
        if(err){
            res.send(""+err.toString());
        }else{
-           res.status(200).send(""+JSON.stringify(result));
+           res.status(200).send(""+JSON.stringify(result.rows));
        }
        
     });
