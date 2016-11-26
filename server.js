475e082afda56dee0a0179a2d886476e5ca96f53
Senthil Vidhiyakar <svidhiyakar@gmail.com>
2016-11-26 20:12:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ var app = express();
 
 app.use(morgan('combined'));
 app.use(bodyParser.json());
-
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(session({
     secret : 'someRandomSecretValue',
     cookie : {
